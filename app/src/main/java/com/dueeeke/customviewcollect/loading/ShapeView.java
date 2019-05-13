db985e395489e01c11285aaae98a63b4818765a8
qy <1356512059@qq.com>
2019-05-13 17:32:31 +0800

    loading view

--- app/src/main/java/com/dueeeke/customviewcollect/loading/ShapeView.java
+++ app/src/main/java/com/dueeeke/customviewcollect/loading/ShapeView.java
@@ -11,7 +11,16 @@ import android.view.View;
 
 public class ShapeView extends View {
 
-    private Paint mPaint;
+    private Paint mTrianglePaint, mCirclePaint, mRectPaint;
+
+    private Path mTriAnglePath;
+
+    public static final int SHAPE_CIRCLE = 1;
+    public static final int SHAPE_RECT = 2;
+    public static final int SHAPE_TRIANGLE = 3;
+
+
+    private int mCurrentShape = SHAPE_TRIANGLE;
 
     public ShapeView(Context context) {
         this(context, null);
@@ -27,19 +36,60 @@ public class ShapeView extends View {
     }
 
     private void init() {
-        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaint.setColor(Color.BLACK);
+        mTrianglePaint = getPaint(Color.RED);
+        mCirclePaint = getPaint(Color.GREEN);
+        mRectPaint = getPaint(Color.BLUE);
+
+        mTriAnglePath = new Path();
     }
 
 
+    public Paint getPaint(int color) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(color);
+        return paint;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(Math.min(width, height), Math.min(width, height));
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
-        Path path = new Path();
-        path.moveTo(20, 100);
-        path.lineTo(50, 20);
-        path.lineTo(100, 100);
-        path.close();
-        canvas.drawPath(path, mPaint);
 
+        float size = getMeasuredWidth();
+
+        switch (mCurrentShape) {
+            case SHAPE_RECT:
+                canvas.drawRect(0, 0, size, size, mRectPaint);
+                break;
+            case SHAPE_CIRCLE:
+                canvas.drawCircle(size / 2, size / 2, size / 2, mCirclePaint);
+                break;
+            case SHAPE_TRIANGLE:
+                mTriAnglePath.moveTo(size / 2, 0);
+                mTriAnglePath.lineTo(0, (float) (size / 2 * Math.sqrt(3)));
+                mTriAnglePath.lineTo(size, (float) (size / 2 * Math.sqrt(3)));
+                mTriAnglePath.close();
+                canvas.drawPath(mTriAnglePath, mTrianglePaint);
+                break;
+        }
+    }
+
+    public void changeShape() {
+        mCurrentShape++;
+
+        if (mCurrentShape == 4) {
+            mCurrentShape = SHAPE_CIRCLE;
+        }
+
+        invalidate();
+    }
+
+    public int getCurrentShape() {
+        return mCurrentShape;
     }
 }
